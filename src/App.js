33872c6fdca84ebffeade0034919d3d8fa3cf726
adamzul <adamzulkarnain98@gmail.com>
2019-07-23 22:43:50 +0700

    init. menyiapkan gambar untuk item dan beberapa css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">1</div>
+        <div className="col-6">2</div>
+      </div>
     </div>
+
   );
 }
 
