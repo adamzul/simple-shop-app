c6e10c2d91f0e40e5d939c922dd1d6c62a62c7a9
adamzul <adamzulkarnain98@gmail.com>
2019-07-30 15:53:41 +0700

    membuat component buttonprovider css nya dinamis. menampilkan detail product pada component detail. menambah css untuk component product agar lebih responsif serta menambahkan tipe data untuk product. sementara mengubah detailproduct menjadi satu item saja

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import {ProductConsumer} from '../context';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
 	render() {
@@ -35,6 +36,16 @@ export default class Product extends Component {
 	}
 }
 
+Product.propTypes = {
+	product: PropTypes.shape({
+		id: PropTypes.number,
+		img: PropTypes.string,
+		title: PropTypes.string,
+		price: PropTypes.number,
+		inCart: PropTypes.bool
+	}).isRequired
+}
+
 const ProductWrapper = styled.div`
 	card {
 		border-color: transparent;
@@ -48,8 +59,43 @@ const ProductWrapper = styled.div`
 
 	&:hover {
 		.card {
-			border: 0.04rem solid rgba(0,0,0,0.2);
-			box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+			border: 0.04rem solid rgba(0, 0, 0, 0.2);
+			box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
 		}
+		.card-footer {
+			background: rgba(247, 247, 247)
+		}
+	}
+
+	.img-container {
+		position: relative;
+		overflow: hidden;
+	}
+
+	.img-container:hover .card-img-top {
+		transform: scale(1.2);
+	}
+	.card-img-top {
+		transition: all 0.1s linear;
+	}
+	.cart-btn {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		padding: 0.2rem 0.4rem;
+		background: var(--lightBlue);
+		border: none;
+		color: var(--mainWhite);
+		font-size: 1.4rem;
+		border-radius: 0.5rem 0 0 0;
+		transform: translate(100%, 0);
+		transition: all 0.1s linear;
+	}
+	.img-container:hover .cart-btn {
+		transform: translate(0, 0);
+	}
+	.cart-btn:hover {
+		color: var(--mainBlue);
+		cursor: pointer;
 	}
 `
--- src/data.js
+++ src/data.js
@@ -100,7 +100,82 @@ export const storeProducts = [
 	},
 ];
 
-export const detailProduct = [
+// export const detailProduct = [
+// 	{
+// 		id: 1,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+// 	},
+// 	{
+// 		id: 2,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+// 	},
+// 	{
+// 		id: 3,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+// 	},
+// 	{
+// 		id: 4,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+// 	},
+// 	{
+// 		id: 5,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// 	},
+// 	{
+// 		id: 6,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// 	},
+// 	{
+// 		id: 7,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// 	},
+// 	{
+// 		id: 8,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// 	},
+// 	{
+// 		id: 9,
+// 		title: 'samsung',
+// 		img: 'img/samsung01.jpg',
+// 		price: "100",
+// 		company: "Samsung",
+// 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// 	},
+// ]
+
+export const detailProduct = 
 	{
 		id: 1,
 		title: 'samsung',
@@ -108,69 +183,4 @@ export const detailProduct = [
 		price: "100",
 		company: "Samsung",
 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	},
-	{
-		id: 2,
-		title: 'samsung',
-		img: 'img/samsung01.jpg',
-		price: "100",
-		company: "Samsung",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	},
-	{
-		id: 3,
-		title: 'samsung',
-		img: 'img/samsung01.jpg',
-		price: "100",
-		company: "Samsung",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	},
-	{
-		id: 4,
-		title: 'samsung',
-		img: 'img/samsung01.jpg',
-		price: "100",
-		company: "Samsung",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	},
-	{
-		id: 5,
-		title: 'samsung',
-		img: 'img/samsung01.jpg',
-		price: "100",
-		company: "Samsung",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	},
-	{
-		id: 6,
-		title: 'samsung',
-		img: 'img/samsung01.jpg',
-		price: "100",
-		company: "Samsung",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	},
-	{
-		id: 7,
-		title: 'samsung',
-		img: 'img/samsung01.jpg',
-		price: "100",
-		company: "Samsung",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	},
-	{
-		id: 8,
-		title: 'samsung',
-		img: 'img/samsung01.jpg',
-		price: "100",
-		company: "Samsung",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	},
-	{
-		id: 9,
-		title: 'samsung',
-		img: 'img/samsung01.jpg',
-		price: "100",
-		company: "Samsung",
-		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	},
-]
+	}
